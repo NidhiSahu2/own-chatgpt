44036312c0cccf227b7471bc9cc63b3da0b4a92c
Nidhi sahu <149890086+NidhiSahu2@users.noreply.github.com>
2023-11-16 23:38:12 +0530

    Add files via upload

---
+++ ownchatgpt.py
@@ -0,0 +1,27 @@
+# own chatgpt using owndata #
+question  = {
+    
+ 
+    "hi": " hey i am chatgpt",
+    "how are you" : "i am good",
+    "do want any help": " yes i want help",
+    " what kind of help" : " pelease  tell me about  this topic    hear is solutions !!!",
+    "how are you" : " i am openai i also will and about you",
+    "i am very well" : " ok ok",
+    " what is your name" : " my name is openai",
+    " how old are you" : "i am 20 year old ",
+    " which one is your fav song " : " my song maan meri jaan",
+    " tell me about ownself" : "  i am chatgpt and i also curious for user help to explain his/her answer a ot of question of user i want i also best provide this my jod i have a lot of data so we can hlep any user easliy",
+    "which plACE Is spcial in jabalpur" : " kachnar city the temple of godesss shiva",
+    " which one fav food jabalpur" : " panipuri",
+
+
+     
+} 
+while True:
+    qs= input()
+
+    if(qs=="quit"):
+        break
+    else:
+        print(question[qs])
